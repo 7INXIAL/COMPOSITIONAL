89dd8328f059fb37197b1e6e618c99863bb15899
林溪 <7INXIAL@gmail.com>
2023-03-03 15:35:28 +0800

    Update module.js

--- plop-templates/module.js
+++ plop-templates/module.js
@@ -10,32 +10,32 @@ export default {
   actions: [
     {
       type: "add",
-      path: "src/modules/{{name}}/locale/zh_CN.ts",
+      path: "src/modules/{{lowerCase name}}/locale/zh_CN.ts",
       templateFile: "plop-templates/module/locale/zh_CN.hbs",
     },
     {
       type: "add",
-      path: "src/modules/{{name}}/locale/en_US.ts",
+      path: "src/modules/{{lowerCase name}}/locale/en_US.ts",
       templateFile: "plop-templates/module/locale/en_US.hbs",
     },
     {
       type: "add",
-      path: "src/modules/{{name}}/views/index.vue",
+      path: "src/modules/{{lowerCase name}}/views/index.vue",
       templateFile: "plop-templates/module/views/index.hbs",
     },
     {
       type: "add",
-      path: "src/modules/{{name}}/route.ts",
+      path: "src/modules/{{lowerCase name}}/route.ts",
       templateFile: "plop-templates/module/route.hbs",
     },
     {
       type: "add",
-      path: "src/modules/{{name}}/service.ts",
+      path: "src/modules/{{lowerCase name}}/service.ts",
       templateFile: "plop-templates/module/service.hbs",
     },
     {
       type: "add",
-      path: "src/modules/{{name}}/store.ts",
+      path: "src/modules/{{lowerCase name}}/store.ts",
       templateFile: "plop-templates/module/store.hbs",
     },
   ],
